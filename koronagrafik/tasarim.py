2589a190064441dfcbb6f028de863ff48137c763
berkay bakacak <59742076+berkaybakacak@users.noreply.github.com>
2022-01-21 02:13:35 +0300

    Update tasarim.py

--- koronagrafik/tasarim.py
+++ koronagrafik/tasarim.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 
+"""
+Koronagrafik
+Son güncelleme tarihi: 21 Ocak 2022 Cuma 01:19
+Yazar: Berkay Bakacak.
+"""
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 
-"""
-Koronagrafik
-Son güncelleme tarihi: 14 Ocak 2021 Perşembe 17:19
-Yazar: Berkay Bakacak.
-"""
-
 class Ayarlar(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(200, 100)
+        self.setFixedSize(250, 100)
         self.arayuzuAyarla()
 
     def arayuzuAyarla(self):
         self.setWindowTitle("Ayarlar")
+        self.setStyleSheet("background-color: #ececeb;")
 
         self.baslarkenkoyutema = QtWidgets.QCheckBox("Açılırken koyu temayı etkinleştir")
         self.baslarkensutungrafik = QtWidgets.QCheckBox("Açılışta sütun grafiği seç")
@@ -37,24 +38,85 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.resize(1000, 700)
-        self.setWindowTitle("Koronavirüs günlük bulaşma/ölüm grafiği")
+        self.setWindowTitle("Koronavirüs günlük yeni vaka/yeni ölü grafiği")
+        self.setStyleSheet("background-color: #ececeb;")
 
         self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
+        self.canvas.fig.set_facecolor("#ececeb")
+        self.canvas.axes[0].set_facecolor("#ececeb")
+        self.canvas.axes[1].set_facecolor("#ececeb")
+
+        self.butonStiller = {1:["""
+        QPushButton {
+            color: black;
+            background-color: white;
+            border-radius: 5px;
+            width: 110px;
+            height: 20px;
+            border: 1px solid darkgray;
+        }
+
+        QPushButton:pressed {
+            background-color: gray;
+            color: white;
+        }
+        """,
+        """
+        QPushButton {
+            color: white;
+            background-color: darkgray;
+            border-radius: 5px;
+            width: 110px;
+            height: 20px;
+            border: 1px solid white;
+        }
+
+        QPushButton:pressed {
+            background-color: gray;
+            color: black;
+        }
+        """]}
+        self.butonStiller[2] = []
+        self.butonStiller[2].append(self.butonStiller[1][0].replace("width: 110px;", "width: 200px;"))
+        self.butonStiller[2].append(self.butonStiller[1][1].replace("width: 110px;", "width: 200px;"))
+
+
+
+        self.comboBoxStil = ["""
+        QComboBox {
+            width: 110px;
+            height: 20px;
+            border-radius: 5px;
+            border: 1px solid darkgray;
+            color: black;
+            background-color: white;
+        }
+        """,
+        """
+        QComboBox {
+            width: 110px;
+            height: 20px;
+            border-radius: 5px;
+            border: 1px solid white;
+            color: white;
+            background-color: black;
+        }
+        """]
 
         ustBolum = QtWidgets.QHBoxLayout()
 
+
         self.geriButon = QtWidgets.QPushButton("Geri")
-        self.geriButon.setStyleSheet("background-color: white; color black;")
+        self.geriButon.setStyleSheet(self.butonStiller[1][0])
         ustBolum.addWidget(self.geriButon)
 
-        self.ulkelerListesi = QtWidgets.QComboBox()
-        self.ulkelerListesi.setStyleSheet("background-color: white; color black;")
 
+        self.ulkelerListesi = QtWidgets.QComboBox()
+        self.ulkelerListesi.setStyleSheet(self.comboBoxStil[0])
         ustBolum.addWidget(self.ulkelerListesi)
 
         self.ileriButon = QtWidgets.QPushButton("İleri")
-        self.ileriButon.setStyleSheet("background-color: white; color black;")
-        
+        self.ileriButon.setStyleSheet(self.butonStiller[1][0])
         ustBolum.addWidget(self.ileriButon)
 
         self.bildiriEtiket = QtWidgets.QLabel("Son güncelleme tarihi: ")
@@ -63,37 +125,40 @@ class MainWindow(QtWidgets.QMainWindow):
         ustBolum.addStretch()
 
         self.cizgiGrafik = QtWidgets.QRadioButton("Çizgi grafik")
-        self.cizgiGrafik.setStyleSheet("background-color: white; color black;")
+        self.cizgiGrafik.setStyleSheet("background-color: #ececeb; color black;")
 
         self.sutunGrafik = QtWidgets.QRadioButton("Sütun grafik")
-        self.sutunGrafik.setStyleSheet("background-color: white; color black;")
+        self.sutunGrafik.setStyleSheet("background-color: #ececeb; color black;")
 
         ustBolum.addWidget(self.cizgiGrafik)
         ustBolum.addWidget(self.sutunGrafik)
 
         self.ulkeyiGuncelle = QtWidgets.QPushButton("Ülkeyi güncelle")
-        self.ulkeyiGuncelle.setStyleSheet("background-color: white; color black;")
+        self.ulkeyiGuncelle.setStyleSheet(self.butonStiller[1][0])
         ustBolum.addWidget(self.ulkeyiGuncelle)
 
         self.grafigikaydet = QtWidgets.QPushButton("Kaydet")
-        self.grafigikaydet.setStyleSheet("background-color: white; color black;")
+        self.grafigikaydet.setStyleSheet(self.butonStiller[1][0])
         ustBolum.addWidget(self.grafigikaydet)
 
         guncelleButonLayout = QtWidgets.QHBoxLayout()
-        self.tumUlkeleriGuncelle = QtWidgets.QPushButton("Tüm ülkeleri güncelle")
-        self.tumUlkeleriGuncelle.setStyleSheet("background-color: white; color black;")
+        self.tumUlkeleriGuncelle = QtWidgets.QPushButton("Tüm ülkelerin verilerini güncelle")
+        self.tumUlkeleriGuncelle.setStyleSheet(self.butonStiller[2][0])
+        
+
         guncelleButonLayout.addStretch()
         guncelleButonLayout.addWidget(self.tumUlkeleriGuncelle)
         guncelleButonLayout.addStretch()
 
         self.altTaraf = QtWidgets.QHBoxLayout()
-        self.etiket = QtWidgets.QLabel("2021 - Berkay Bakacak. sürüm: 3.0")
+        self.etiket = QtWidgets.QLabel("2021 - 2022. Berkay Bakacak. sürüm: 3.4")
         self.altTaraf.addWidget(self.etiket)
         self.altTaraf.addStretch()
         self.koyuTema = QtWidgets.QCheckBox("Koyu tema")
 
         self.ayarlar = QtWidgets.QPushButton()
         self.ayarlar.setIcon(QtGui.QIcon("ayarlar.png"))
+        self.ayarlar.setStyleSheet("border-radius: 100%; background-color: transparent;")
 
         self.altTaraf.addWidget(self.koyuTema)
         self.altTaraf.addWidget(self.ayarlar)
